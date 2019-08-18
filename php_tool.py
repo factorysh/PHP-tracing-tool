de75b91bd67c273af30e0fc007b3adb004173425
Vagrant Default User <vagrant@buster.localdomain>
2019-08-18 21:11:15 +0000

    printing problems fixed

--- php_tool.py
+++ php_tool.py
@@ -18,6 +18,12 @@ class c:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+# globals
+usdt_tab = []
+syscalls = ["socket", "socketpair", "bind", "listen", "accept", "accept4", "connect", "getsockname", "getpeername", "sendto", "recvfrom", "setsockopt", "getsockopt", "shutdown", "sendmsg", "sendmmsg", "recvmsg", "recvmmsg", "read", "write", "sendfile64"]
+#syscalls = []
+PAD = " "
+
 # get the pid in arg
 parser = argparse.ArgumentParser(
     description="php_tool",
@@ -35,7 +41,6 @@ struct call_t {
     char    clazz[80];              // class name
     char    method[80];             // method name
     char    file[80];               // php file name
-    long    sys_ret;                // syscall function return
 };
 
 #define SYS 1
@@ -57,24 +62,15 @@ int NAME(struct pt_regs *ctx) {
     READ_CLASS
     READ_METHOD
     READ_FILE
-    bpf_probe_read(&data.clazz, sizeof(data.clazz), (void *)clazz);
-    bpf_probe_read(&data.method, sizeof(data.method), (void *)method);
-    bpf_probe_read(&data.file, sizeof(data.file), (void *)file);
+    bpf_probe_read_str(&data.clazz, sizeof(data.clazz), (void *)clazz);
+    bpf_probe_read_str(&data.method, sizeof(data.method), (void *)method);
+    bpf_probe_read_str(&data.file, sizeof(data.file), (void *)file);
 
+    
     #ifndef IS_RETURN
     
     u64 time = bpf_ktime_get_ns();
     start_func.update(&method, &time);
-
-    #endif
-    #ifdef IS_RETURN
-
-    u64 *start_ns = start_func.lookup(&method);
-    if (!start_ns) {
-        return 0;
-    }
-    data.lat = bpf_ktime_get_ns() - *start_ns;
-    start_func.delete(&method);
 
     #endif
 
@@ -83,6 +79,19 @@ int NAME(struct pt_regs *ctx) {
     depth = entry.lookup_or_init(&data.pid, &zero);
     data.depth = DEPTH;
     UPDATE
+
+    #ifdef IS_RETURN
+
+    u64 *start_ns = start_func.lookup(&method);
+    if (!start_ns) {
+        calls.perf_submit(ctx, &data, sizeof(data));
+        start_func.delete(&method);
+        return 0;
+    }
+    data.lat = bpf_ktime_get_ns() - *start_ns;
+    start_func.delete(&method);
+
+    #endif
 
     calls.perf_submit(ctx, &data, sizeof(data));
     return 0;
@@ -108,30 +117,30 @@ TRACEPOINT_PROBE(syscalls, sys_exit_SYSCALL) {
     }
 
     u64 *depth, zero = 0, clazz = 0, method = 0;
-    u64 *start_ns = start.lookup(&pid);
-    if (!start_ns) {
-        return 0;
-    }
 
     struct call_t data = {};
-    data.lat = bpf_ktime_get_ns() - *start_ns;
     data.type = SYS;
     data.pid = pid;
-    data.sys_ret = args->ret;
     depth = entry.lookup_or_init(&data.pid, &zero);
     data.depth = *depth;
     char method_str[80] = "SYSCALL";
     bpf_probe_read_str(&data.method, sizeof(data.method), method_str);
     char class_str[80] = "sys";
     bpf_probe_read_str(&data.clazz, sizeof(data.clazz), class_str);
+
+    u64 *start_ns = start.lookup(&pid);
+    if (!start_ns) {
+        calls.perf_submit(args, &data, sizeof(data));
+        return 0;
+    }
+
+    data.lat = bpf_ktime_get_ns() - *start_ns;
     calls.perf_submit(args, &data, sizeof(data));
     return 0;
 }
 """
 
-usdt_tab = []
-
-# Fonction pour activer un probe
+# function for trace a php probe
 def enable_probe(probe_name, func_name, read_class, read_method, read_file, is_return):
     global program, php_trace_template, usdt
     if is_return:
@@ -149,6 +158,18 @@ def enable_probe(probe_name, func_name, read_class, read_method, read_file, is_r
         usdt_tab.append(usdt)
         usdt.enable_probe_or_bail(probe_name, func_name)
 
+# function for trace a syscall
+def enable_syscall_tracepoint(sys_name):
+    global program
+    condition = ""
+    for i, pid in enumerate(args.pid):
+        condition += "pid >> 32 != %s" % (str(pid))
+        if i < len(args.pid) - 1:
+            condition += " && "
+    new_template = sys_trace_template.replace("PID_CONDITION", condition)
+    program += new_template.replace("SYSCALL", sys_name)
+
+# trace php function entry and return
 enable_probe("function__entry", "php_entry",
     "bpf_usdt_readarg(4, ctx, &clazz);",
     "bpf_usdt_readarg(1, ctx, &method);",
@@ -158,28 +179,17 @@ enable_probe("function__return", "php_return",
     "bpf_usdt_readarg(1, ctx, &method);",
     "bpf_usdt_readarg(2, ctx, &file);", is_return=True)
 
-# syscalls
-#depth = "*depth + 1" if not is_return else "*depth | (1ULL << 63)"
-#update = "++(*depth);" if not is_return else "if (*depth) --(*depth);"
-
-syscalls = ["socket", "socketpair", "bind", "listen", "accept", "accept4", "connect", "getsockname", "getpeername", "sendto", "recvfrom", "setsockopt", "getsockopt", "shutdown", "sendmsg", "sendmmsg", "recvmsg", "recvmmsg", "read", "write", "sendfile64"]
-
-condition = ""
-for i, pid in enumerate(args.pid):
-    condition += "pid >> 32 != %s" % (str(pid))
-    if i < len(args.pid) - 1:
-        condition += " && "
-
-new_template = sys_trace_template.replace("PID_CONDITION", condition)
+# trace the syscalls in the list
 for sys in syscalls:
-    program += new_template.replace("SYSCALL", sys)
+    enable_syscall_tracepoint(sys)
 
-#print(program)
-# Charger le programme dans eBPF
+print(program)
+
+# inject the C program generated in eBPF
 bpf = BPF(text=program, usdt_contexts=usdt_tab)
 
 print("php tool, pid = %s... Ctrl-C to quit." % (args.pid))
-print("%-6s %-16s %s" % ("PID", "LAT", "METHOD"))
+print("%-6s %-6s %s" % ("PID", "LAT", "METHOD"))
 
 # Classe pour refleter la struct C
 class CallEvent(ct.Structure):
@@ -191,7 +201,6 @@ class CallEvent(ct.Structure):
         ("clazz", ct.c_char * 80),
         ("method", ct.c_char * 80),
         ("file", ct.c_char * 80),
-        ("sys_ret", ct.c_long)
         ]
 
 total_lat = 0
@@ -203,25 +212,28 @@ def print_event(cpu, data, size):
     # syscalls
     if event.type == 1:
         total_lat += event.lat
-        print("%-6d %-16u %s : %ld" % (event.pid >> 32, event.lat, ("  " * (depth - 1)) + event.clazz.decode("utf-8", "replace") + "." + c.BLUE + event.method.decode("utf-8", "replace") + c.ENDC, event.sys_ret))    
+        print("%-6d %-6u %-40s" % (event.pid >> 32, event.lat, (PAD * (depth - 1)) + event.clazz.decode("utf-8", "replace") + "." + c.BLUE + event.method.decode("utf-8", "replace") + c.ENDC))    
     # php function
     else:
         if event.depth & (1 << 63):
             direction = "<- "
-            print("%-6d %-16u %s" % (event.pid >> 32, total_lat, ("  " * (depth - 1)) + c.BLUE + "traced syscalls total latence" + c.ENDC))    
-            total_lat = 0
-            if event.method == "main":
-                exit()
+            if syscalls and total_lat > 0:
+                print("%-6d %-6u %-40s" % (event.pid >> 32, total_lat, (PAD * (depth - 1)) + c.BLUE + "traced syscalls total latence" + c.ENDC))    
+                total_lat = 0
         else:
             direction = "-> "
-        print("%-6d %-16s %s" % (
+        print("%-6d %-6s %-40s" % (
                 event.pid >> 32,
                 str(event.lat) if event.lat > 0 else "-",
-                ("  " * (depth - 1)) + direction + event.clazz.decode('utf-8', 'replace') + "." + event.method.decode('utf-8', 'replace') + " " + c.UNDERLINE + "from " + event.file + c.ENDC
+                (PAD * (depth - 1)) + direction + event.clazz.decode('utf-8', 'replace') + "." + event.method.decode('utf-8', 'replace') + " " + c.UNDERLINE + "from " + event.file + c.ENDC
             )
         )
+        if event.depth & (1 << 63) and  event.method == "main" and depth == 1:
+            exit()
 
-bpf["calls"].open_perf_buffer(print_event)
+        # Avec wordpress (un truc complexe) l'affichage est trop lourd. Idee : affichage basique -> pas de syscalls ni le nom de fichier et apres l'execution faire top 10 des fonctions les plus slow et syscalls pareil (avec full info cette fois, nom de fichier, retour, args etc)
+
+bpf["calls"].open_perf_buffer(print_event, page_cnt=4096)
 while 1:
     try:
         bpf.perf_buffer_poll()
